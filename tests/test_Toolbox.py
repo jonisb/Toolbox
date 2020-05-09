6484b0d8738e6674388b56d580a10e0484d47c61
jonisb <github.com@JsBComputing.se>
2020-05-09 19:56:37 +0200

    Create test_Toolbox.py
    Adds first test

---
+++ tests/test_Toolbox.py
@@ -0,0 +1,13 @@
+﻿# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+import unittest
+
+
+class test_SettingsClass(unittest.TestCase):
+    def test_create_empty_object(self):
+        from jsbc.Toolbox import SettingsClass
+        settings = SettingsClass()
+        assert type(settings) == SettingsClass
+        assert settings.export() == {}
+        assert settings.export(True) == {}
