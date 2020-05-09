36d3e0bfa5d8087e9e2963ebfb306dc6bfe9fc3b
jonisb <github.com@JsBComputing.se>
2020-05-09 23:43:27 +0200

    Update test_Toolbox.py
    More basic SettingsClass tests

--- tests/test_Toolbox.py
+++ tests/test_Toolbox.py
@@ -11,3 +11,44 @@ class test_SettingsClass(unittest.TestCase):
         assert type(settings) == SettingsClass
         assert settings.export() == {}
         assert settings.export(True) == {}
+
+    def test_create_simple_object_with_default_value(self):
+        from jsbc.Toolbox import SettingsClass
+        settings = SettingsClass({'name': 'test'})
+        assert settings.export() == {}
+        assert settings.export(True) == {'name': 'test'}
+
+    def test_create_simple_object_addDefault_value(self):
+        from jsbc.Toolbox import SettingsClass
+        settings = SettingsClass()
+        settings.addDefault({'name': 'test'})
+        assert settings.export() == {}
+        assert settings.export(True) == {'name': 'test'}
+
+    def test_create_simple_object_with_default_value_new_data(self):
+        from jsbc.Toolbox import SettingsClass
+        settings = SettingsClass({'name': 'test'}, {'name': 'test2'})
+        assert settings.export() == {'name': 'test2'}
+
+    def test_create_simple_object_with_default_value_addData(self):
+        from jsbc.Toolbox import SettingsClass
+        settings = SettingsClass({'name': 'test'})
+        settings.addData({'name': 'test2'})
+        assert settings.export() == {'name': 'test2'}
+
+    def test_create_simple_object_with_deep_default_value(self):
+        from jsbc.Toolbox import SettingsClass
+        settings = SettingsClass({'name': {'name2': 'test2'}})
+        assert settings.export() == {'name': {}}, settings.export()
+        assert settings.export(True) == {'name': {'name2': 'test2'}}
+
+    def test_create_simple_object_with_deep_default_value_deep_data(self):
+        from jsbc.Toolbox import SettingsClass
+        settings = SettingsClass({'name': {'name2': 'test2'}}, {'name': {'name2': 'test3'}})
+        assert settings.export() == {'name': {'name2': 'test3'}}
+        assert settings.export(True) == {'name': {'name2': 'test3'}}
+        settings['name']['name2'] = 'test4'
+        assert settings.export() == {'name': {'name2': 'test4'}}
+        settings['name']['name2'] = 'test2'
+        assert settings.export() == {'name': {}}, settings.export()
+        assert settings.export(True) == {'name': {'name2': 'test2'}}
