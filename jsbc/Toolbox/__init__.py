bb7c23ee1fb4a3b039c63c9fd7c7e9e822b49fbe
jonisb <github.com@JsBComputing.se>
2020-05-09 20:14:36 +0200

    Update __init__.py
    Imports OrderedDict

--- jsbc/Toolbox/__init__.py
+++ jsbc/Toolbox/__init__.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals, division, absolute_import
 
+from collections import OrderedDict
+
 __version__ = '0.0.0'
+
+
+class SettingsClass(OrderedDict):
+    pass
