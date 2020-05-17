2fb93a2db3840b999e38621b06c7a18163ee1d77
jonisb <github.com@JsBComputing.se>
2020-05-17 03:02:35 +0200

    Update __init__.py
    Use "jsbc.compat" to import OrderedDict

--- jsbc/Toolbox/__init__.py
+++ jsbc/Toolbox/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals, division, absolute_import
 
-from collections import OrderedDict
+from jsbc.compat.OrderedDict import OrderedDict
 
 __version__ = '0.0.0'
 
